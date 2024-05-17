5d18a50706e67d9b0b69d0810e9f4cc9ac2d0dba
Farido01 <faridtuyghunov@gmail.com>
2024-05-18 01:25:03 +0500

    cpp

---  dp.cpp
+++  dp.cpp
@@ -1,6 +1,24 @@
 #include<iostream>
-#include<vector>
+#include<fstream> // Добавляем заголовочный файл для работы с файлами
 using namespace std;
-int main(){
-    cout<<"Hello world!";
+
+int main() {
+    // Открываем файл для записи
+    ofstream outputFile("output.txt");
+    
+    // Проверяем, успешно ли открылся файл
+    if (!outputFile.is_open()) {
+        cout << "Unable to open file";
+        return 1;
+    }
+    
+    // Записываем текстовое сообщение в файл
+    outputFile << "Hello world!";
+    
+    // Закрываем файл
+    outputFile.close();
+    
+    cout << "Data written to file successfully";
+    
+    return 0;
 }
